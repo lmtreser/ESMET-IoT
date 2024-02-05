d4bd4150d4844ca6e7a03cefd651390747fb0f03
lmtreser <lmtreser@gmail.com>
2024-02-05 16:31:48 -0300

    update fw credentials

--- firmware/fw_esmet/Credentials.h
+++ firmware/fw_esmet/Credentials.h
@@ -2,18 +2,18 @@
 #define ARDUINO_CREDENTIALS_H
 
 // Credenciales WiFi para conectarse a Internet
-#define STA_SSID "..."
-#define STA_PASS "..."
+#define STA_SSID "wifi_ssid"
+#define STA_PASS "wifi_password"
 
 // Credenciales del broker MQTT
-#define MQTT_BROKER       "maqiatto.com"
+#define MQTT_BROKER       "mqtt_broker_host"
 #define MQTT_BROKER_PORT  1883
-#define MQTT_USERNAME     "lmtreser@gmail.com"
-#define MQTT_KEY          "teyz"
+#define MQTT_USERNAME     "mqtt_broker_username"
+#define MQTT_KEY          "mqtt_broker_password"
 
 // Lista de topicos
-#define TOPIC1    "lmtreser@gmail.com/invernadero1/sensor1"
-#define TOPIC2    "lmtreser@gmail.com/invernadero1/sensor2"
-#define TOPIC3    "lmtreser@gmail.com/invernadero1/alarma"
+#define TOPIC1    "topic/invernadero1/sensor1"
+#define TOPIC2    "topic/invernadero1/sensor2"
+#define TOPIC3    "topic/invernadero1/alarma"
 
 #endif /* ARDUINO_CREDENTIALS_H */
